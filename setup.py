8c894d6b5ef386af9ff81349c9c552e8c55a3228
Ivan Mičetić <ivan.micetic@gmail.com>
2023-05-13 00:00:41 +0200

    Bump to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="selenium_helpers",
-    version="0.0.3",
+    version="0.1.0",
     author="Ivan Mičetić",
     author_email="ivan.micetic@gmail.com",
     description="Helper functions for selenium",
